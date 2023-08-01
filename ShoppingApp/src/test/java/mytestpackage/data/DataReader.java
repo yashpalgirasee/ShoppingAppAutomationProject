af3d798ad93bd1cfdd007cd973107810672e4c37
Yashpal Girase <yashpalgirase10@gmail.com>
2023-08-01 15:52:39 +0530

    added DataReader file

---
+++ ShoppingApp/src/test/java/mytestpackage/data/DataReader.java
@@ -0,0 +1,27 @@
+package mytestpackage.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DataReader {
+
+	
+	public void getJasonDataToMap() throws IOException {
+		
+		// read jason to String
+		String jsonContent = FileUtils.readFileToString(new File(System.getProperty("user.dir")+"src\\test\\java\\mytestpackage\\data\\purchaseOrder.json"), StandardCharsets.UTF_8);
+		
+		// string to Hashmap
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String , String>> data = mapper.readValue(jsonContent, new TypeReference <List<HashMap<String , String>>>(){});
+	}
+
+}
