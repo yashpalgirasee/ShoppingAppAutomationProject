5d2d8ce1b203c10939e7381fab27e09c68a64b7c
Yashpal Girase <yashpalgirase10@gmail.com>
2023-08-03 17:50:12 +0530

    added method for Screeshot in basetest class

--- ShoppingApp/src/test/java/mytestpackage/testcomponent/BaseTest.java
+++ ShoppingApp/src/test/java/mytestpackage/testcomponent/BaseTest.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -58,6 +60,18 @@ public class BaseTest{
 		
 		return driver;
 	
+	}
+	
+	public String getScreenshot(String testCaseName) throws IOException {
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+		
+		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png" ;
+		
+		
 	}
 	
 	public List<HashMap<String,String>> getJasonDataToMap(String filePath) throws IOException {
